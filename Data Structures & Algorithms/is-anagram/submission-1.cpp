d2b6d8b54047e721209df59f555a0a736bc6a073
Peter Ukrainetz <peterukrainetz@gmail.com>
2026-08-24 17:45:27 -0700

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length())
+            return false;
+
+        unordered_map<char, int> count;
+
+        for (int i = 0; i < s.length(); i++)
+        {
+            count[s[i]]++;
+            count[t[i]]--;
+        }
+
+        for (auto& pair : count)
+        {
+            if (pair.second != 0)
+                return false;
+        }
+
+        return true;
+    }
+};
