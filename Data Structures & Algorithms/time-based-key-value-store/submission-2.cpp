8d2ca23134f29664a711be131581998e5fbb19a3
Peter Ukrainetz <peterukrainetz@gmail.com>
2026-09-09 17:46:57 -0700

    Add: time-based-key-value-store - submission-2

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-2.cpp
@@ -0,0 +1,39 @@
+class TimeMap {
+public:
+    TimeMap() {
+        
+    }
+    
+    void set(string key, string value, int timestamp) {
+        store[key].push_back(pair{timestamp, value});
+    }
+    
+    string get(string key, int timestamp) {
+        if (!store.contains(key))
+            return "";
+
+        int l = 0, r = store[key].size() - 1;
+
+        string result = "";
+
+        while (l <= r)
+        {
+            int mid = l + (r - l) / 2;
+
+            if (store[key][mid].first > timestamp)
+                r = mid - 1;
+            else if (store[key][mid].first < timestamp)
+            {
+                result = store[key][mid].second;
+                l = mid + 1;
+            }
+            else
+                return store[key][mid].second;
+        }
+
+        return result;
+    }
+
+private:
+    unordered_map<string, vector<pair<int, string>>> store;
+};
